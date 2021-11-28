8648a5f74458650daa48e93d7d7001820ca77587
cloud441 <kevin.guillet@epita.fr>
2021-11-28 19:27:40 +0100

    [BACKEND]: update config labels with more matching

--- modules/config/label_match.py
+++ modules/config/label_match.py
@@ -1,5 +1,17 @@
 label_match_dict= {
+        'PROPNOM': 'a1a',
+        'PROPNOM2': 'a2a',
+        'PROPNOM3': 'a3a',
+        'DEMANDEA': 'a4a',
+        'DEMANDELE': 'a5a',
         'DEPT': 'b1',
         'COMMUNE': 'b2',
-        'PREFIXE1': 'P1'
+        'PREFIXE': 'b3',
+        'SECTION': 'b4',
+        'FEUILLE': 'b5',
+        #NUMDOSSIER ?
+        #NM d'ordre ? date de reception ?
+        'PV': 'b8',
+        'DELIM': 'b9',
+        'AUTRE': 'b10'
         }
