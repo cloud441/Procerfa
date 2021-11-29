f4637b722e583770d209501c85d1441a44f44181
cloud441 <kevin.guillet@epita.fr>
2021-11-29 11:02:20 +0100

    [BACKEND]: make file path options of the run file

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
+
 from modules import config
 from modules.cerfaReader import CerfaReader
 from modules.cerfaWriter import CerfaWriter
 
 
-def main():
-    reader = CerfaReader("data/pdf_filled/old_sample.pdf")
-    writer = CerfaWriter("output_test/output.pdf", config.label_match_dict)
+def parse_arg():
+
+    parser = argparse.ArgumentParser(description='Personal information')
+    parser.add_argument('-i', '--input', dest='input_file', type=str, help='path to the input Cerfa file to read.')
+    parser.add_argument('-o', '--output', dest='output_file', type=str, help='path to the output Cerfa file to write.')
+
+    return parser.parse_args()
+
+
+def main() -> None:
+
+    args = parse_arg()
+    if (not args.input_file) or (not args.output_file):
+        print("Error: you need to give the input and ouput file for processing.")
+        return
+
+    reader = CerfaReader(args.input_file)
+    writer = CerfaWriter(args.output_file, config.label_match_dict)
 
     writer.annotate(reader.get_annot_dict())
 
