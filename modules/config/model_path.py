7659dc454f92c6922ff212429d77a3b98cd7e023
cloud441 <kevin.guillet@epita.fr>
2021-12-22 20:42:10 +0100

    [WINDOWS]: fix path according to OS

--- modules/config/model_path.py
+++ modules/config/model_path.py
@@ -1,9 +1,9 @@
 import os
 
 # Linux dev:
-dir_path = f"data{os.sep}pdf_models{os.sep}"
+#dir_path = f"data{os.sep}pdf_models{os.sep}"
 
 # Windows release:
-#dir_path = f"..{os.sep}..{os.sep}data{os.sep}pdf_models{os.sep}"
+dir_path = f"..{os.sep}..{os.sep}data{os.sep}pdf_models{os.sep}"
 
 model_path = dir_path + "new_model.pdf"
